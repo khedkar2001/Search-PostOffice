3984a4bcead65dbe54a5ec10a626619422f62e65
Saurabh Khedkar <70061591+khedkar2001@users.noreply.github.com>
2025-08-26 09:05:54 +0530

    script.js

--- script.js
+++ script.js
@@ -1,83 +1,96 @@
-navigator.geolocation.getCurrentPosition(
-    function (position) {
-        //    initMap(position.coords.latitude, position.coords.longitude)
-        let lat = position.coords.latitude;
-        let long = position.coords.longitude;
+let yourIp;
+let allPostOffices = [];
+document.addEventListener("DOMContentLoaded", function () {
+  // Fetch the IP address from the API
+  fetch("https://api.ipify.org?format=json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Display the IP address on the screen
+      document.getElementById("your-ip-address").textContent = data.ip;
+      yourIp = data.ip;
+    })
+    .catch((error) => {
+      console.error("Error fetching IP address:", error);
+    });
+});
 
-        const api_key = '6005bef9fa3d4a3e914a11e9ee4a94af';
+document.getElementById("btn").addEventListener("click", () => {
+  // console.log("button clicked");
+  fetch(`https://ipapi.co/${yourIp}/json/`)
+    .then((response) => response.json())
+    .then((data) => {
+      // Display the IP address on the screen
+      // document.getElementById("your-ip-address").textContent = data.ip;
+      // yourIp = data.ip;
+      console.log("fetch", data);
+      document.getElementById("ip-address").textContent = `${yourIp}`;
+      document.getElementById("lat").textContent = `Latitude: ${data.latitude}`;
+      document.getElementById(
+        "long"
+      ).textContent = `Longitude: ${data.longitude}`;
+      document.getElementById("city").textContent = `City: ${data.city}`;
+      document.getElementById("region").textContent = `Region: ${data.region}`;
+      document.getElementById("org").textContent = `Organisation: ${data.org}`;
+      document.getElementById("hostname").textContent = `Hostname: ${data.org}`;
 
-        const container_div = document.getElementById("container");
+      //update g map
+      document.getElementById(
+        "currentLocation"
+      ).innerHTML = `<iframe src="https://maps.google.com/maps?q=${data.latitude}, ${data.longitude}&z=15&output=embed" width="600" height="450" frameborder="0" style="border:0"></iframe>`;
 
-        var requestOptions = {
-            method: 'GET',
-        };
+      //update more information
+      document.getElementById(
+        "timezone"
+      ).textContent = `Time Zone : ${data.timezone}`;
+      document.getElementById(
+        "dateAndTime"
+      ).textContent = `Date and Time : ${data.timezone}`;
+      document.getElementById(
+        "pincode"
+      ).textContent = `Pincode : ${data.postal}`;
 
-        // feting data using api to show in the white-container
-        fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${long}&apiKey=${api_key}`, requestOptions)
-            .then(response => response.json())
-            .then(result => container_div.innerHTML = `<p>Name Of Time Zone : ${result.features[0].properties.timezone.name}</p>
-            <span class="flex"><span>Lat: ${result.features[0].properties.lat}</span>
-            <span>Long: ${result.features[0].properties.lon}</span></span>
-            <p>Offset STD: ${result.features[0].properties.timezone.offset_STD}</p>
-            <p>Offset STD Seconds : ${result.features[0].properties.timezone.offset_STD_seconds}</p>
-            <p>Offset DST : ${result.features[0].properties.timezone.offset_DST}</p>
-            <p>Offset DST Seconds: ${result.features[0].properties.timezone.offset_DST_seconds}</p>
-            <p>Country: ${result.features[0].properties.country}</p>
-            <p>Postcode: ${result.features[0].properties.postcode}</p>
-            <p>City: ${result.features[0].properties.city}</p>`)
-            .catch(error => console.log('error', error));
-    }
-)
+      document.getElementById("homePage").style.display = "none";
+      document.getElementById("content").style.display = "block";
 
-let btn = document.getElementById("btn");
+      return data.postal;
+    })
+    .then((postalCode) => {
+      fetch(`https://api.postalpincode.in/pincode/${postalCode}`)
+        .then((response) => response.json())
+        .then((postOfficeData) => {
+          allPostOffices = postOfficeData[0].PostOffice;
+          console.log("post offcie daat", postOfficeData);
+          // const postOffices = postOfficeData[0].PostOffice;
+          displayPostOffices(allPostOffices);
+        });
+    })
+    .catch((error) => {
+      console.error("Error fetching IP address:", error);
+    });
+});
 
-btn.addEventListener("click", function () {
-    const search_bar = document.getElementById("search-bar");
-    const enteredAddress = search_bar.value.trim();
+function displayPostOffices(postOffices) {
+  const container = document.getElementById("post-offices");
+  container.innerHTML = "";
+  postOffices.forEach((postOffice) => {
+    container.innerHTML += renderPostOfficeCard(postOffice);
+  });
+}
 
-    // Validate the entered address
-    if (enteredAddress === "") {
-        alert("Please enter a valid address.");
-        return;
-    }
+function renderPostOfficeCard(postOffice) {
+  return `<div class="card">
+          <div>${postOffice.Name}</div>
+          <div>${postOffice.BranchType}</div>
+          <div>${postOffice.DeliveryStatus}</div>
+          <div>${postOffice.District}</div>
+          <div>${postOffice.Division}</div>
+        </div>`;
+}
 
-
-    // Retrieve Timezone by Address
-    const api_key = '6005bef9fa3d4a3e914a11e9ee4a94af';
-    const container_2 = document.getElementById("container-2");
-
-    var requestOptions = {
-        method: 'GET',
-    };
-
-    fetch(`https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(enteredAddress)}&apiKey=${api_key}`, requestOptions)
-        .then(response => response.json())
-        .then(result => {
-            const firstResult = result.features[0];
-            const lat = firstResult.geometry.coordinates[1];
-            const lon = firstResult.geometry.coordinates[0];
-
-            // Fetch timezone based on the coordinates
-            fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&apiKey=${api_key}`, requestOptions)
-                .then(response => response.json())
-                .then(timezoneResult => {
-                    let data = timezoneResult.features[0].properties;
-                    container_2.innerHTML = `<p>Name Of Time Zone : ${data.timezone.name}</p>
-                    <span class="flex"><span>Lat: ${data.lat}</span>
-                    <span>Long: ${data.lon}</span></span>
-                    <p>Offset STD: ${data.timezone.offset_STD}</p>
-                    <p>Offset STD Seconds : ${data.timezone.offset_STD_seconds}</p>
-                    <p>Offset DST : ${data.timezone.offset_DST}</p>
-                    <p>Offset DST Seconds: ${data.timezone.offset_DST_seconds}</p>
-                    <p>Country: ${data.country}</p>
-                    <p>Postcode: ${data.postcode}</p>
-                    <p>City: ${data.city}</p>`;
-                    // console.log(timezoneResult)
-
-                    document.getElementById("hidden-container").style.display="block";
-                })
-                .catch(error => console.log('error', error));
-        })
-        .catch(error => console.log('error', error));
-    
-})
+document.getElementById("searchInput").addEventListener("input", function () {
+  const query = this.value.toLowerCase();
+  const filtered = allPostOffices.filter((po) => {
+    return po.Name.toLowerCase().includes(query);
+  });
+  displayPostOffices(filtered);
+});
